3e2a615be45bafaf1918f5d2fa7b2b3ea8ac76b2
Chana Kupetz <chanakupetz@gmail.com>
2020-07-24 03:56:22 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,10 @@
 def find_element_index(array, value_to_find)
-  array.include?(value_to_find)
-
+  count = 0
+  while count < array.length do 
+    
+    count +=1
+  end
 end
 
 #if true- return the index of it. if false return nil
+#  array.include?(value_to_find)
